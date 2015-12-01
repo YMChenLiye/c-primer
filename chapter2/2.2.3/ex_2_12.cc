2c776ed68931ab005e24e867253f8954d9b13717
ymcly <ymchenliye@gmail.com>
2015-12-01 20:48:17 +0800

    finish section 2.2.3

---
+++ chapter2/2.2.3/ex_2_12.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main()
+{
+//	int double=3.14;  error: expected unqualified-id before ‘=’ token
+	int _;
+//	int catch-22;   error: expected unqualified-id before ‘catch’
+//	int 1_or_2 =1; invalid suffix "_or_2" on integer constant
+	double Double =3.14;
+
+	return 0;
+}
