b7c289dd6c7b7baf2071bf36acd6bac60856651e
ymcly <ymchenliye@gmail.com>
2015-12-02 22:46:07 +0800

    finish section 2.4.4

---
+++ chapter2/2.4.4/ex_2_32.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main ()
+{
+//	int null=0,*p=null; ex_2_32.cc:5:16: error: invalid conversion from ‘int’ to ‘int*’ [-fpermissive]
+	int *p=0;//this can run successfully
+
+	return 0;
+
+}
