80371d1b4cb755c42c156b517e7c663747ce4dc2
ymcly <ymchenliye@gmail.com>
2015-12-09 21:01:13 +0800

    finish section 4.2

---
+++ chapter4/4.2/ex_4_6.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main()
+{
+	int tmp;
+	cin>>tmp;
+	if(tmp % 2)
+	{
+		cout<<"odd"<<endl;
+	}
+	else
+	{
+		cout<<"even"<<endl;
+	}
+
+	return 0;
+}
---
+++ chapter4/4.2/ex_4_7.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main()
+{
+	unsigned short i=0;i--;
+	unsigned int j=0;j--;
+	unsigned long k=0;k--;
+
+
+	cout<<i<<" "<<j<<" "<<k<<endl;
+
+	return 0;
+}
