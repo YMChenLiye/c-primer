e89e594343b3d79d54a4a885812733e206b8449d
ymcly <ymchenliye@gmail.com>
2015-12-10 22:16:43 +0800

    finish section 4.10

---
+++ chapter4/4.10/ex_4_31.cc
@@ -0,0 +1,1 @@
+there is no difference in this problem
---
+++ chapter4/4.10/ex_4_32.cc
@@ -0,0 +1,1 @@
+ptr and ix have the same function. The former use a pointer, and the latter use the index of array. we use the loop to through the array.(just choose one from ptr and ix)
