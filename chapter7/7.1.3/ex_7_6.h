3a1512e7a6816fb8f4cee6da1f7aec72f453adc9
ymcly <ymchenliye@gmail.com>
2015-12-21 20:59:35 +0800

    edit ex_7_6.h

--- chapter7/7.1.3/ex_7_6.h
+++ chapter7/7.1.3/ex_7_6.h
@@ -2,42 +2,33 @@
 #define ex_7_6_h
 #include <iostream>
 #include <string>
-
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-
 struct Sales_data
 {
 	string bookNo;
 	unsigned units_sold;
 	double revenue;
-
 	string isbn() const
 	{
 		return bookNo;
 	}
-
 	Sales_data& combine(const Sales_data&);
-
 };
-
 Sales_data &Sales_data::combine(const Sales_data& rhs)
 {
 	units_sold += rhs.units_sold;
 	revenue += rhs.revenue;
 	return *this;
 }
-
-
 Sales_data add(const Sales_data &lhs,const Sales_data &rhs)
 {
 	Sales_data sum=lhs;
 	sum.combine(rhs);
 	return sum;
 }
-
 std::istream &read(std::istream &is,Sales_data &item)
 {
 	double price=0;
@@ -45,11 +36,9 @@ std::istream &read(std::istream &is,Sales_data &item)
 	item.revenue=item.units_sold * price;
 	return is;
 }
-
 std::ostream &print(std::ostream &os,const Sales_data item)
 {
 	cout<<item.isbn()<<" "<<item.units_sold<<" "<<item.revenue;
 	return os;
 }
-
 #endif
