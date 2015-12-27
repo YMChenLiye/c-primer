779f31ae194b4ad1953b353a0375bab58344ed42
ymcly <ymchenliye@gmail.com>
2015-12-27 15:13:36 +0800

    finish section 7.5.6

---
+++ chapter7/7.5.6/ex_7_54.cc
@@ -0,0 +1,1 @@
+shouldn't, cause a constexpr function must contain exactly one return statement.
---
+++ chapter7/7.5.6/ex_7_55.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <type_traits>
+
+struct Data {
+	int ival;
+	std::string s;
+};
+
+int main()
+{
+//	std::cout << std::boolalpha;
+	std::cout << std::is_literal_type<Data>::value << std::endl;
+	// output: false
+ }
+	
