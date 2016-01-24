186e7f201918ff8277c49d3a633b99e3d50e487b
ymcly <ymchenliye@gmail.com>
2016-01-24 21:11:58 +0800

    finish section 14.2.2

---
+++ chapter14/14.2.2/ex_14_10.cc
@@ -0,0 +1,4 @@
+
+(a) correct format.
+(b) ilegal input. But 0-210-99999-9 will be converted to a float stored in this object. As a result, the data inside will be a wrong one. Output: 10 24 22.8 0.95
+
