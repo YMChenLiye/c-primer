2b9ed1c0982c9bf86c388b062860d5ddcd5f1100
ymcly <ymchenliye@gmail.com>
2016-01-24 22:08:04 +0800

    finish section 14.4

---
+++ chapter14/14.4/Sales_data2.h
@@ -0,0 +1,71 @@
+#ifndef ex_14_2_h
+#define ex_14_2_h
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+using std::istream;
+using std::ostream;
+class Sales_data{
+	friend istream& operator>>(istream&,Sales_data&);
+	friend ostream& operator<<(ostream&,const Sales_data&);
+	friend Sales_data operator+(const Sales_data&,const Sales_data&);
+	friend Sales_data& operator+=(const Sales_data&,const Sales_data&);
+	public:
+		Sales_data(const string &s,unsigned n,double p):bookNo(s),units_sold(n),revenue(n*p) { }
+		Sales_data():Sales_data("",0,0.0f) { }
+		Sales_data(const string &s):Sales_data(s,0,0.0f) { }
+		Sales_data(istream&);
+		
+		string isbn() const { return bookNo;}
+
+	private:
+		double avg_price()const { return units_sold? revenue/units_sold : 0;}
+		string bookNo;
+		unsigned units_sold=0;
+		double revenue=0.0;
+};
+Sales_data operator+(const Sales_data&lhs,const Sales_data &rhs)
+{
+	Sales_data result;
+	result.bookNo=lhs.bookNo;
+	result.units_sold=lhs.units_sold+rhs.units_sold;
+	result.revenue=lhs.revenue+rhs.revenue;
+	return result;
+}
+Sales_data& operator+=(const Sales_data& rhs)
+{
+	*this=*this+rhs;
+	return *this;
+}
+
+istream& operator>>(istream &is,Sales_data& item)
+{
+	double price;
+	is>>item.bookNo>>item.units_sold>>price;
+	if(is)
+	{
+		item.revenue=price*item.units_sold;
+	}
+	else
+	{
+		item=Sales_data();
+	}
+	return is;
+}
+Sales_data::Sales_data(istream& is)
+{
+	is>>*this;
+}
+
+ostream& operator<<(ostream& os,const Sales_data& item)
+{
+	os<<item.bookNo<<" "<<item.units_sold<<" "<<item.revenue<<" "<<item.avg_price();
+	return os;
+}
+
+
+
+#endif
---
+++ chapter14/14.4/ex_14_21.cc
@@ -0,0 +1,1 @@
+Disadvantages: Both + and +=, uses an temporary object of Sales_data. But it is no need for that.
