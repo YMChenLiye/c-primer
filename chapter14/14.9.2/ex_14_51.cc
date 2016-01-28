a79e6ca216eb80a92817851611a0b4d6e9171d5d
ymcly <ymchenliye@gmail.com>
2016-01-28 18:38:58 +0800

    finish section 14.9.2

---
+++ chapter14/14.9.2/ex_14_51.cc
@@ -0,0 +1,10 @@
+best viable function: void calc(int). cause class-type conversion is the lowest ranked.
+
+review the order:
+
+exact match
+const conversion
+promotion
+arithmetic or pointer conversion
+class-type conversion
+
