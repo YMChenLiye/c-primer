d55156c40bba724a1cc022896452287a17c95dd0
ymcly <ymchenliye@gmail.com>
2016-01-30 19:20:45 +0800

    finish section 15.2.3

---
+++ chapter15/15.2.3/ex_15_10.cc
@@ -0,0 +1,1 @@
+The function takes a std::istream from which std::ifstream is derived. Hence the ifstream object "is a" istream , which is why it works.
---
+++ chapter15/15.2.3/ex_15_9.cc
@@ -0,0 +1,1 @@
+The static type of a pointer or reference to a base class may differ from its dynamic type. Anything like this can be an example.
