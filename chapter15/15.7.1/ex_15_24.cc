cc6ffc5202ccd3d7e04b767cb83de84150efaf6f
ymcly <ymchenliye@gmail.com>
2016-01-31 19:29:49 +0800

    finish section 15.7.1

---
+++ chapter15/15.7.1/ex_15_24.cc
@@ -0,0 +1,3 @@
+Generally, a base class should define a virtual destructor.
+
+The destructor needs to be virtual to allow objects in the inheritance hierarchy to be dynamically allocated and destroyed.
