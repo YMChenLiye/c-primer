a53d75ab756e520a2bb10f8b9b76bd6735ae06ae
ymcly <ymchenliye@gmail.com>
2016-02-13 23:11:46 +0800

    finish section 16.1.5

---
+++ chapter16/16.1.5/ex_16_25.cc
@@ -0,0 +1,9 @@
+/**
+ *  @brief  instantiation declaration. The definition of it is somewhere else
+ */
+//extern template class vector<string>;
+
+/**
+ *  @brief  instantiation definition. The compiler will generate codes for it.
+ */
+//template class vector<Sales_data>;
---
+++ chapter16/16.1.5/ex_16_26.cc
@@ -0,0 +1,1 @@
+http://stackoverflow.com/questions/21525169/while-explicitly-instantiating-vectorsometype-what-is-the-sometype-default-co
