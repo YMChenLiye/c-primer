24c69aebc5d2cfe49475717f249fe0c5531af21e
ymcly <ymchenliye@gmail.com>
2016-02-24 21:32:18 +0800

    finish section 16.2.6

---
+++ chapter16/16.2.6/ex_16_46.cc
@@ -0,0 +1,4 @@
+//
+//// In each iteration, the dereference operator * returns a lvalue which is changed to rvalue by
+//// std::move , becasue the member function construct takes rvalue reference rather than lvalue
+//// reference.
