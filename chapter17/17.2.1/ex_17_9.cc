1ba37a2ef1a0f63758556c51294dea1a25a733cd
ymcly <ymchenliye@gmail.com>
2016-02-26 21:02:37 +0800

    finish section 17.2.1

---
+++ chapter17/17.2.1/ex_17_9.cc
@@ -0,0 +1,10 @@
+// Exercise 17.9: Explain the bit pattern each of the following bitset objects contains:
+// //     (a) bitset<64> bitvec(32);
+// //          0000000000000000000000000000000000000000000000000000000000100000
+// //                                      //                           ^
+// //     (b) bitset<32> bv(1010101);
+// //          00000000000011110110100110110101
+// //          // note that the argument here is decemal
+// //     (c) string bstr; cin >> bstr; bitset<8>bv(bstr);
+// //          depends on what has been input.
+// //
